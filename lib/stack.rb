3bea74e76a22cd84807213a7355539067c62247f
Adam DePaolo <amdepaolo@gmail.com>
2023-04-02 19:17:55 -0400

    commit tree 3befbb7b4ff54cea4ed3b68dc013963cdb6c81c7

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,43 @@
 # your code here
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if self.full?
+            raise 'Stack Overflow'
+        else @stack.push(value)
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.length == 0
+    end
+
+    def full?
+        @stack.length == @limit
+    end
+
+    def search (value)
+        spot = @stack.rindex(value)
+        if spot
+            self.size - spot -1
+        else -1
+        end
+    end
+end
